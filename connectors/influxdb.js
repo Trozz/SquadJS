21d33ada2e60e4c590fe7b61cd623d6f5558ac4b
Thomas Smyth <thomas.smyth3141@gmail.com>
2020-04-02 18:31:13 +0100

    Prevent InfluxDB attempting to connect when not in use

--- connectors/influxdb.js
+++ connectors/influxdb.js
@@ -10,105 +10,106 @@ import {
 } from 'core/config';
 
 class InfluxDBConnector {
-  constructor() {
-    this.influx = new Influx.InfluxDB({
-      host: INFLUXDB_HOST,
-      port: INFLUXDB_PORT,
-      username: INFLUXDB_USERNAME,
-      password: INFLUXDB_PASSWORD,
-      database: INFLUXDB_DATABASE,
-      schema: [
-        {
-          measurement: 'ServerTickRate',
-          fields: {
-            tick_rate: Influx.FieldType.FLOAT
-          },
-          tags: ['server']
-        },
-        {
-          measurement: 'PlayerCount',
-          fields: {
-            player_count: Influx.FieldType.INTEGER
-          },
-          tags: ['server']
-        },
-        {
-          measurement: 'Match',
-          fields: {
-            dlc: Influx.FieldType.STRING,
-            mapClassname: Influx.FieldType.STRING,
-            layerClassname: Influx.FieldType.STRING,
-            map: Influx.FieldType.STRING,
-            layer: Influx.FieldType.STRING
-          },
-          tags: ['server']
-        },
-        {
-          measurement: 'PlayerWound',
-          fields: {
-            victim: Influx.FieldType.STRING,
-            victimName: Influx.FieldType.STRING,
-            victimTeamID: Influx.FieldType.INTEGER,
-            victimSquadID: Influx.FieldType.INTEGER,
-            attacker: Influx.FieldType.STRING,
-            attackerName: Influx.FieldType.STRING,
-            attackerTeamID: Influx.FieldType.INTEGER,
-            attackerSquadID: Influx.FieldType.INTEGER,
-            damage: Influx.FieldType.STRING,
-            weapon: Influx.FieldType.STRING,
-            teamkill: Influx.FieldType.BOOLEAN
-          },
-          tags: ['server']
-        },
-        {
-          measurement: 'PlayerDie',
-          fields: {
-            victim: Influx.FieldType.STRING,
-            victimName: Influx.FieldType.STRING,
-            victimTeamID: Influx.FieldType.INTEGER,
-            victimSquadID: Influx.FieldType.INTEGER,
-            attacker: Influx.FieldType.STRING,
-            attackerName: Influx.FieldType.STRING,
-            attackerTeamID: Influx.FieldType.INTEGER,
-            attackerSquadID: Influx.FieldType.INTEGER,
-            damage: Influx.FieldType.STRING,
-            weapon: Influx.FieldType.STRING,
-            teamkill: Influx.FieldType.BOOLEAN
-          },
-          tags: ['server']
-        },
-        {
-          measurement: 'Revive',
-          fields: {
-            victim: Influx.FieldType.STRING,
-            victimName: Influx.FieldType.STRING,
-            victimTeamID: Influx.FieldType.INTEGER,
-            victimSquadID: Influx.FieldType.INTEGER,
-            attacker: Influx.FieldType.STRING,
-            attackerName: Influx.FieldType.STRING,
-            attackerTeamID: Influx.FieldType.INTEGER,
-            attackerSquadID: Influx.FieldType.INTEGER,
-            damage: Influx.FieldType.STRING,
-            weapon: Influx.FieldType.STRING,
-            teamkill: Influx.FieldType.BOOLEAN,
-            reviver: Influx.FieldType.STRING,
-            reviverName: Influx.FieldType.STRING,
-            reviverTeamID: Influx.FieldType.INTEGER,
-            reviverSquadID: Influx.FieldType.INTEGER
-          },
-          tags: ['server']
-        }
-      ]
-    });
-
-    this.points = [];
-    setInterval(() => {
-      this.influx.writePoints(this.points);
-      this.points = [];
-    }, INFLUXDB_WRITE_INTERVAL);
-  }
-
   writePoint(point) {
+    // setup influx if not done so already
+    if (!this.influx) {
+      this.influx = new Influx.InfluxDB({
+        host: INFLUXDB_HOST,
+        port: INFLUXDB_PORT,
+        username: INFLUXDB_USERNAME,
+        password: INFLUXDB_PASSWORD,
+        database: INFLUXDB_DATABASE,
+        schema: [
+          {
+            measurement: 'ServerTickRate',
+            fields: {
+              tick_rate: Influx.FieldType.FLOAT
+            },
+            tags: ['server']
+          },
+          {
+            measurement: 'PlayerCount',
+            fields: {
+              player_count: Influx.FieldType.INTEGER
+            },
+            tags: ['server']
+          },
+          {
+            measurement: 'Match',
+            fields: {
+              dlc: Influx.FieldType.STRING,
+              mapClassname: Influx.FieldType.STRING,
+              layerClassname: Influx.FieldType.STRING,
+              map: Influx.FieldType.STRING,
+              layer: Influx.FieldType.STRING
+            },
+            tags: ['server']
+          },
+          {
+            measurement: 'PlayerWound',
+            fields: {
+              victim: Influx.FieldType.STRING,
+              victimName: Influx.FieldType.STRING,
+              victimTeamID: Influx.FieldType.INTEGER,
+              victimSquadID: Influx.FieldType.INTEGER,
+              attacker: Influx.FieldType.STRING,
+              attackerName: Influx.FieldType.STRING,
+              attackerTeamID: Influx.FieldType.INTEGER,
+              attackerSquadID: Influx.FieldType.INTEGER,
+              damage: Influx.FieldType.STRING,
+              weapon: Influx.FieldType.STRING,
+              teamkill: Influx.FieldType.BOOLEAN
+            },
+            tags: ['server']
+          },
+          {
+            measurement: 'PlayerDie',
+            fields: {
+              victim: Influx.FieldType.STRING,
+              victimName: Influx.FieldType.STRING,
+              victimTeamID: Influx.FieldType.INTEGER,
+              victimSquadID: Influx.FieldType.INTEGER,
+              attacker: Influx.FieldType.STRING,
+              attackerName: Influx.FieldType.STRING,
+              attackerTeamID: Influx.FieldType.INTEGER,
+              attackerSquadID: Influx.FieldType.INTEGER,
+              damage: Influx.FieldType.STRING,
+              weapon: Influx.FieldType.STRING,
+              teamkill: Influx.FieldType.BOOLEAN
+            },
+            tags: ['server']
+          },
+          {
+            measurement: 'Revive',
+            fields: {
+              victim: Influx.FieldType.STRING,
+              victimName: Influx.FieldType.STRING,
+              victimTeamID: Influx.FieldType.INTEGER,
+              victimSquadID: Influx.FieldType.INTEGER,
+              attacker: Influx.FieldType.STRING,
+              attackerName: Influx.FieldType.STRING,
+              attackerTeamID: Influx.FieldType.INTEGER,
+              attackerSquadID: Influx.FieldType.INTEGER,
+              damage: Influx.FieldType.STRING,
+              weapon: Influx.FieldType.STRING,
+              teamkill: Influx.FieldType.BOOLEAN,
+              reviver: Influx.FieldType.STRING,
+              reviverName: Influx.FieldType.STRING,
+              reviverTeamID: Influx.FieldType.INTEGER,
+              reviverSquadID: Influx.FieldType.INTEGER
+            },
+            tags: ['server']
+          }
+        ]
+      });
+
+      this.points = [];
+      setInterval(() => {
+        this.influx.writePoints(this.points);
+        this.points = [];
+      }, INFLUXDB_WRITE_INTERVAL);
+    }
+
     this.points.push(point);
   }
 }
